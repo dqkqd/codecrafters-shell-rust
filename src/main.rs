43c35c0d6f8deb248d237795079fc59c45a9aa54
Khanh Duong <dqkqdlot@gmail.com>
2025-01-20 20:49:04 +0900

    echo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,62 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::{
+    error, fmt,
+    io::{self, Write},
+    num::ParseIntError,
+};
+
+// TODO: lifetime
+enum Command {
+    Exit(i32),
+    Echo(String),
+    Invalid(String),
+}
+
+#[derive(Debug)]
+enum CommandError {
+    ParseIntError(ParseIntError),
+}
+
+impl fmt::Display for CommandError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            CommandError::ParseIntError(ref err) => write!(f, "ParseIntError: {}", err),
+        }
+    }
+}
+
+impl error::Error for CommandError {}
+
+impl From<ParseIntError> for CommandError {
+    fn from(err: ParseIntError) -> CommandError {
+        CommandError::ParseIntError(err)
+    }
+}
+
+impl TryFrom<String> for Command {
+    type Error = CommandError;
+
+    fn try_from(command: String) -> Result<Command, CommandError> {
+        let command = command.trim();
+        if let Some(code) = command.strip_prefix("exit ") {
+            let code = code.parse()?;
+            Ok(Command::Exit(code))
+        } else if let Some(echo) = command.strip_prefix("echo ") {
+            Ok(Command::Echo(echo.into()))
+        } else {
+            Ok(Command::Invalid(command.into()))
+        }
+    }
+}
+
+impl Command {
+    fn execute(self) {
+        match self {
+            Command::Exit(code) => std::process::exit(code),
+            Command::Echo(s) => println!("{}", s),
+            Command::Invalid(s) => println!("{}: command not found", s),
+        }
+    }
+}
 
 fn main() {
     // Uncomment this block to pass the first stage
@@ -12,10 +69,7 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        let input = input.trim_end();
-        if input == "exit 0" {
-            std::process::exit(0);
-        }
-        println!("{}: command not found", input.trim_end());
+        let cmd: Command = input.try_into().unwrap();
+        cmd.execute();
     }
 }
