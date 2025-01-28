6a9640b158e5640cbcf52c3f62b5a899986a2965
Khanh Duong <dqkqdlot@gmail.com>
2025-01-28 22:58:41 +0900

    beep for unmatched completion

--- src/parser/completer.rs
+++ src/parser/completer.rs
@@ -7,15 +7,16 @@ pub(crate) fn completed_suffix(
     stdout: &mut StdoutLock<'static>,
     pat: &str,
 ) -> Result<Option<String>> {
-    let completed_candidates = completed_candidates(pat);
-    let result = match completed_candidates.len() {
-        0 => None,
-        1 => completed_candidates[0]
-            .strip_prefix(pat)
-            .map(|s| s.to_string()),
-        _ => {
+    let result = match completed_candidates(pat) {
+        CompletedCandidates::None => {
+            stdout.write_all(b"\x07")?;
+            stdout.flush()?;
+            None
+        }
+        CompletedCandidates::One(candidate) => candidate.strip_prefix(pat).map(|s| s.to_string()),
+        CompletedCandidates::Multiple(candidates) => {
             stdout.write_all(b"\r\n")?;
-            for s in completed_candidates {
+            for s in candidates {
                 stdout.write_all(s.as_bytes())?;
                 stdout.write_all(b"\r\n")?;
             }
@@ -27,12 +28,24 @@ pub(crate) fn completed_suffix(
     Ok(result)
 }
 
-fn completed_candidates(pat: &str) -> Vec<String> {
-    builtins()
+enum CompletedCandidates {
+    None,
+    One(String),
+    Multiple(Vec<String>),
+}
+
+fn completed_candidates(pat: &str) -> CompletedCandidates {
+    let mut candidates: Vec<String> = builtins()
         .iter()
         .filter(|s| s.starts_with(pat))
         .map(|s| s.to_string())
-        .collect()
+        .collect();
+
+    match candidates.len() {
+        0 => CompletedCandidates::None,
+        1 => CompletedCandidates::One(candidates.remove(0)),
+        _ => CompletedCandidates::Multiple(candidates),
+    }
 }
 
 fn builtins() -> Vec<&'static str> {
